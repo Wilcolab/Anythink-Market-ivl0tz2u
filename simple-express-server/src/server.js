f023a75e653e2269aa4a9da8484b3f81d4cf3b7d
Folayan Donald Michael <47295878+donfolayan@users.noreply.github.com>
2025-08-11 19:59:36 +0000

    Update server listen address to allow external connections

--- simple-express-server/src/server.js
+++ simple-express-server/src/server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 8001;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
